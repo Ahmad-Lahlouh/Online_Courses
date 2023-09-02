f7eab1df5a511e8c67b5d5590a29c4ab2a5e65b7
Ahmad-Lahlouh <ahmd.lh98@gmail.com>
2023-09-02 23:15:22 +0200

    v3

--- src/Modules/Course/Course.router.js
+++ src/Modules/Course/Course.router.js
@@ -15,16 +15,16 @@ router.post('/',auth(endPoint.create),fileUpload(fileValidation.image).fields([
     {name:'mainImage',maxCount:1},
 ]),courseController.createCourse)
 
-router.put('/update/:courseId',auth(endPoint.update),fileUpload(fileValidation.image).fields([
-    {name:'mainImage',maxCount:1},
-    {name:'subImages',maxCount:5}
-]),courseController.updateCourse)
+// router.put('/update/:courseId',auth(endPoint.update),fileUpload(fileValidation.image).fields([
+//     {name:'mainImage',maxCount:1},
+//     {name:'subImages',maxCount:5}
+// ]),courseController.updateCourse)
 
-router.patch('/softDelete/:courseId',auth(endPoint.softDelete),courseController.softDelete)
-router.delete('/forceDelete/:courseId',auth(endPoint.forceDelete),courseController.forceDelete)
-router.patch('/restore/:courseId',auth(endPoint.restore),courseController.restore)
-router.get('/softDelete',auth(endPoint.get),courseController.getSoftDeleteCourses)
-router.get('/:courseId',courseController.getCourse)
-router.get('/',courseController.getCourses)
+// router.patch('/softDelete/:courseId',auth(endPoint.softDelete),courseController.softDelete)
+// router.delete('/forceDelete/:courseId',auth(endPoint.forceDelete),courseController.forceDelete)
+// router.patch('/restore/:courseId',auth(endPoint.restore),courseController.restore)
+// router.get('/softDelete',auth(endPoint.get),courseController.getSoftDeleteCourses)
+// router.get('/:courseId',courseController.getCourse)
+// router.get('/',courseController.getCourses)
 
 export default router
--- src/Modules/Course/controller/Course.controller.js
+++ src/Modules/Course/controller/Course.controller.js
@@ -30,178 +30,178 @@ if(!Course){
 return res.json({message:'sucess',Course})
 })
 
-export const updateCourse = asyncHandler(async (req,res,next)=>{
-    const{CourseId}=req.params
+// export const updateCourse = asyncHandler(async (req,res,next)=>{
+//     const{CourseId}=req.params
 
-    const newCourse = await CourseModel.findById(CourseId)
-    if(!newCourse){
-        return next(new Error('Course not found',{cause:400}))
-    }
-    const {name,price,discount,categoryId,} = req.body
-    if(categoryId){
-        const checkCategory = await categoryModel.findOne({_id:categoryId})
-        if(checkCategory){
-            newCourse.categoryId=categoryId
+//     const newCourse = await CourseModel.findById(CourseId)
+//     if(!newCourse){
+//         return next(new Error('Course not found',{cause:400}))
+//     }
+//     const {name,price,discount,categoryId,} = req.body
+//     if(categoryId){
+//         const checkCategory = await categoryModel.findOne({_id:categoryId})
+//         if(checkCategory){
+//             newCourse.categoryId=categoryId
 
-        }else{
-            return next(new Error('category id or sub category id not found',{cause:400}))
-        }
-    }else if(subCategoryId){
-        const checkSubCategory = await subcategoryModel.findOne({_id:subCategoryId})
-        if(checkSubCategory){
-            newCourse.subCategoryId=subCategoryId
-        }else{
-            return next(new Error('sub category id not found',{cause:400}))
-        }
-    }
-    // if(brandId){
-    //     const checkBrand = await globalErrorHandlerModel.findOne({_id:brandId})
-    //     if(!checkBrand){
-    //         return next(new Error('invalid brand',{cause:400}))
-    //     }else{
-    //         newCourse.brandId=brandId
-    //     }
-    // }
-    if(name){
-        newCourse.name=name
-        newCourse.slug=slugify(name)
-    }
-    if(req.body.description){
-        newCourse.description=req.body.description
-    }
-    if(req.body.stock){
-        newCourse.stock=req.body.stock
-    }
-    if(req.body.colors){
-        newCourse.colors=req.body.colors
-    }
-    if(req.body.sizes){
-        newCourse.sizes=req.body.sizes
-    }
-    if(req.body.sizes){
-        newCourse.sizes=req.body.sizes
-    }
-    if(price && discount){
-        newCourse.finalPrice = price - (price*((discount||0)/100))
-        newCourse.price = price
-        newCourse.discount = discount
-    }else if(price){
-        newCourse.price = price
-        newCourse.finalPrice = price - (price*((newCourse.discount)/100))
-    }else if(discount){
-        newCourse.discount = discount
-        newCourse.finalPrice = newCourse.price - (newCourse.price*((discount)/100))
-    }
-    if(req.files.mainImage.length){
-        const {public_id,secure_url} = await cloudinary.uploader.upload(req.files.mainImage[0].path,{folder:`${process.env.APP_NAME}/Course`})
-        await cloudinary.uploader.destroy(newCourse.mainImage.public_id)
-        newCourse.mainImage.secure_url=secure_url
-        newCourse.mainImage.public_id=public_id
-    }
-    if(req.files.subImages.length){
-        const subImages=[]
+//         }else{
+//             return next(new Error('category id or sub category id not found',{cause:400}))
+//         }
+//     }else if(subCategoryId){
+//         const checkSubCategory = await subcategoryModel.findOne({_id:subCategoryId})
+//         if(checkSubCategory){
+//             newCourse.subCategoryId=subCategoryId
+//         }else{
+//             return next(new Error('sub category id not found',{cause:400}))
+//         }
+//     }
+//     // if(brandId){
+//     //     const checkBrand = await globalErrorHandlerModel.findOne({_id:brandId})
+//     //     if(!checkBrand){
+//     //         return next(new Error('invalid brand',{cause:400}))
+//     //     }else{
+//     //         newCourse.brandId=brandId
+//     //     }
+//     // }
+//     if(name){
+//         newCourse.name=name
+//         newCourse.slug=slugify(name)
+//     }
+//     if(req.body.description){
+//         newCourse.description=req.body.description
+//     }
+//     if(req.body.stock){
+//         newCourse.stock=req.body.stock
+//     }
+//     if(req.body.colors){
+//         newCourse.colors=req.body.colors
+//     }
+//     if(req.body.sizes){
+//         newCourse.sizes=req.body.sizes
+//     }
+//     if(req.body.sizes){
+//         newCourse.sizes=req.body.sizes
+//     }
+//     if(price && discount){
+//         newCourse.finalPrice = price - (price*((discount||0)/100))
+//         newCourse.price = price
+//         newCourse.discount = discount
+//     }else if(price){
+//         newCourse.price = price
+//         newCourse.finalPrice = price - (price*((newCourse.discount)/100))
+//     }else if(discount){
+//         newCourse.discount = discount
+//         newCourse.finalPrice = newCourse.price - (newCourse.price*((discount)/100))
+//     }
+//     if(req.files.mainImage.length){
+//         const {public_id,secure_url} = await cloudinary.uploader.upload(req.files.mainImage[0].path,{folder:`${process.env.APP_NAME}/Course`})
+//         await cloudinary.uploader.destroy(newCourse.mainImage.public_id)
+//         newCourse.mainImage.secure_url=secure_url
+//         newCourse.mainImage.public_id=public_id
+//     }
+//     if(req.files.subImages.length){
+//         const subImages=[]
     
-        for(const file of req.files.subImages){
+//         for(const file of req.files.subImages){
     
-            const {public_id,secure_url} = await cloudinary.uploader.upload(file.path,{folder:`${process.env.APP_NAME}/Course/subImages`})
-            subImages.push({public_id,secure_url})
-            newCourse.subImages=subImages
+//             const {public_id,secure_url} = await cloudinary.uploader.upload(file.path,{folder:`${process.env.APP_NAME}/Course/subImages`})
+//             subImages.push({public_id,secure_url})
+//             newCourse.subImages=subImages
             
-        }
-    }
-    newCourse.updatedBy=req.user._id
-    const Course = await newCourse.save()
-    if(!Course){
-        return next(new Error('fail to update Course',{cause:400}))
-    }
-    return res.json({message:'success',Course})
-})
+//         }
+//     }
+//     newCourse.updatedBy=req.user._id
+//     const Course = await newCourse.save()
+//     if(!Course){
+//         return next(new Error('fail to update Course',{cause:400}))
+//     }
+//     return res.json({message:'success',Course})
+// })
 
-export const getCourse = asyncHandler(async(req,res,next)=>{
-    let{CourseId} = req.params
-    const Course = await CourseModel.findById(CourseId)
-    if(!Course){
-        return next(new Error('Course is not found',{cause:400}))
-    }
-    return res.json({message:'success',Course})
-})
-export const getCourses = asyncHandler(async(req,res,next)=>{
-    let {page,size} = req.query
+// export const getCourse = asyncHandler(async(req,res,next)=>{
+//     let{CourseId} = req.params
+//     const Course = await CourseModel.findById(CourseId)
+//     if(!Course){
+//         return next(new Error('Course is not found',{cause:400}))
+//     }
+//     return res.json({message:'success',Course})
+// })
+// export const getCourses = asyncHandler(async(req,res,next)=>{
+//     let {page,size} = req.query
 
-    if(!page || page<=0){
-        page= 1
-    }
-    if(!size || size<=0){
-        size= 3 
-    }
-    const skip = (page-1)*size
+//     if(!page || page<=0){
+//         page= 1
+//     }
+//     if(!size || size<=0){
+//         size= 3 
+//     }
+//     const skip = (page-1)*size
 
-    const excQuryParams = ['page','size','sort','search']
+//     const excQuryParams = ['page','size','sort','search']
 
-    const filterQuery = {...req.query}
-    excQuryParams.map(params =>{
-        delete filterQuery[params]
-    })
+//     const filterQuery = {...req.query}
+//     excQuryParams.map(params =>{
+//         delete filterQuery[params]
+//     })
 
-    const query = JSON.parse(JSON.stringify(filterQuery).replace(/(gt|gte|lt|lte|in|nin|eq|nqe)/g,match=>`$${match}`))
-    const mongoQuary=CourseModel.find(query).limit(size).skip(skip).sort(req.query.sort?.replaceAll(',',''))
+//     const query = JSON.parse(JSON.stringify(filterQuery).replace(/(gt|gte|lt|lte|in|nin|eq|nqe)/g,match=>`$${match}`))
+//     const mongoQuary=CourseModel.find(query).limit(size).skip(skip).sort(req.query.sort?.replaceAll(',',''))
 
-    if(req.query.find){
+//     if(req.query.find){
 
-        const Courses = await mongoQuary.find({
-            $or:[
+//         const Courses = await mongoQuary.find({
+//             $or:[
     
-                {name:{$regex:req.query.search,$options:'i'}},
-                {description:{$regex:req.query.search,$options:'i'}},
+//                 {name:{$regex:req.query.search,$options:'i'}},
+//                 {description:{$regex:req.query.search,$options:'i'}},
     
-            ]
+//             ]
     
-        })
-        req.body.Courses = Courses
+//         })
+//         req.body.Courses = Courses
 
-    }else{
-        const Courses = await mongoQuary
-        req.body.Courses = Courses
+//     }else{
+//         const Courses = await mongoQuary
+//         req.body.Courses = Courses
 
 
-    }
-    const Courses = req.body.Courses
-    if(!Courses){
-        return next(new Error('Courses is not found',{cause:400}))
-    }
-    return res.json({message:'success',Courses})
-})
-export const softDelete = asyncHandler(async(req,res,next)=>{
-    let {CourseId}=req.params
-    const Course = await CourseModel.findOneAndUpdate({_id:CourseId,isDeleted:false},{isDeleted:true},{new:true})
-    if(!Course){
-        return next(new Error('Course not found',{cause:400}))
-    }
-    return res.json({message:'success',Course})
-})
-export const restore = asyncHandler(async(req,res,next)=>{
-    let {CourseId}=req.params
-    const Course = await CourseModel.findOneAndUpdate({_id:CourseId,isDeleted:true},{isDeleted:false},{new:true})
-    if(!Course){
-        return next(new Error('Course not found',{cause:400}))
-    }
-    return res.json({message:'success',Course})
-})
-export const getSoftDeleteCourses = asyncHandler(async(req,res,next)=>{
-    const Course = await CourseModel.find({isDeleted:true})
-    if(!Course){
-        return next(new Error('Course not found',{cause:400}))
-    }
-    return res.json({message:'success',Course})
-})
-export const forceDelete = asyncHandler(async(req,res,next)=>{
-    let {CourseId}=req.params
-    const Course = await CourseModel.findOneAndDelete({_id:CourseId,isDeleted:true})
-    if(!Course){
-        return next(new Error('Course not found',{cause:400}))
-    }
-    return res.json({message:'success',Course})
-})
+//     }
+//     const Courses = req.body.Courses
+//     if(!Courses){
+//         return next(new Error('Courses is not found',{cause:400}))
+//     }
+//     return res.json({message:'success',Courses})
+// })
+// export const softDelete = asyncHandler(async(req,res,next)=>{
+//     let {CourseId}=req.params
+//     const Course = await CourseModel.findOneAndUpdate({_id:CourseId,isDeleted:false},{isDeleted:true},{new:true})
+//     if(!Course){
+//         return next(new Error('Course not found',{cause:400}))
+//     }
+//     return res.json({message:'success',Course})
+// })
+// export const restore = asyncHandler(async(req,res,next)=>{
+//     let {CourseId}=req.params
+//     const Course = await CourseModel.findOneAndUpdate({_id:CourseId,isDeleted:true},{isDeleted:false},{new:true})
+//     if(!Course){
+//         return next(new Error('Course not found',{cause:400}))
+//     }
+//     return res.json({message:'success',Course})
+// })
+// export const getSoftDeleteCourses = asyncHandler(async(req,res,next)=>{
+//     const Course = await CourseModel.find({isDeleted:true})
+//     if(!Course){
+//         return next(new Error('Course not found',{cause:400}))
+//     }
+//     return res.json({message:'success',Course})
+// })
+// export const forceDelete = asyncHandler(async(req,res,next)=>{
+//     let {CourseId}=req.params
+//     const Course = await CourseModel.findOneAndDelete({_id:CourseId,isDeleted:true})
+//     if(!Course){
+//         return next(new Error('Course not found',{cause:400}))
+//     }
+//     return res.json({message:'success',Course})
+// })
 
 
 
